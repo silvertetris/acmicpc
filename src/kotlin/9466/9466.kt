189be6e4bc0c022b6a0ad47a3bd0864f254820c0
sean <dltjdrnjs3@naver.com>
2025-07-06 20:52:33 +0900

    Update 9466.kt

--- src/kotlin/9466/9466.kt
+++ src/kotlin/9466/9466.kt
@@ -11,15 +11,15 @@ fun main() {
         val visited = BooleanArray(n + 1) { false }
         val cycle = BooleanArray(n + 1) { false }
         var cnt = 0
-        fun dfs(now: Int, visited: BooleanArray,cycle: BooleanArray, adj: IntArray) {
-            visited[now]  = true
-            if(!visited[adj[now]]) {
+        fun dfs(now: Int, visited: BooleanArray, cycle: BooleanArray, adj: IntArray) {
+            visited[now] = true
+            if (!visited[adj[now]]) {
                 dfs(adj[now], visited, cycle, adj)
             }
-            if(!cycle[adj[now]]) { //사이클까지 확인하면 tle뜰거 같음 -> 효율적이게 확인하는 방법?
+            if (!cycle[adj[now]]) { //사이클까지 확인하면 tle뜰거 같음 -> 효율적이게 확인하는 방법?
                 var temp = adj[now]//다음거
                 cnt++
-                while(temp != now) {//사이틀 확인하면 끝 -> cnt만큼 (사이클) 수 차감
+                while (temp != now) {//사이틀 확인하면 끝 -> cnt만큼 (사이클) 수 차감
                     temp = adj[temp]
                     cnt++
                 }
@@ -29,12 +29,12 @@ fun main() {
         for (i in 1..n) {
             adj[i] = arr[i - 1]
         }
-        for(i in 1 .. n) {
-            if(!visited[i]) {
+        for (i in 1..n) {
+            if (!visited[i]) {
                 dfs(i, visited, cycle, adj)
             }
         }
-        bw.write("${n-cnt}\n")
+        bw.write("${n - cnt}\n")
     }
     bw.flush()
     bw.close()
