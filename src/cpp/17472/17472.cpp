235e14ebb7f4d3fa894afea7b2a919e6d53e8b8f
silvertetris <dltjdrnjs3@naver.com>
2025-07-18 04:36:05 +0900

    Update 17472.cpp

--- src/cpp/17472/17472.cpp
+++ src/cpp/17472/17472.cpp
@@ -11,18 +11,16 @@ static int dx[] = {1, 0, -1, 0};
 static int dy[] = {0, 1, 0, -1};
 
 static int map[101][101];
-static bool visited[101][101] = {false,};
+static bool visited[101][101];
 static vector<int> parent;
 static int n, m, sNum;
-static vector<vector<pair<int, int>>> sumlist; //섬 리스트
+static vector<vector<pair<int, int>>> sumlist;
 static vector<pair<int, int>> mlist;
 
-typedef struct Edge {
-    int s, e, v;//start, end , value
-    bool operator > (const Edge& temp) const {
-        return v> temp.v; //value 기준으로 정렬
-    }
-} Edge;
+struct Edge {
+    int s, e, v;
+    bool operator >(const Edge &t) const { return v > t.v; }
+};
 
 static priority_queue<Edge, vector<Edge>, greater<Edge>> pq;
 
@@ -30,121 +28,81 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    cin>> n >> m;
-    for(int i =0; i<n; i++) {
-        for(int j=0; j<m; j++) {
+    cin >> n >> m;
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
             cin >> map[i][j];
-        }
-    }
-
     sNum = 1;
-
-    for(int i =0; i<n; i++ ) {
-        for(int j=0; j<m; j++) {
-            if(map[i][j] == 1 && visited[i][j] != true) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (map[i][j] == 1 && !visited[i][j]) {
                 bfs(i, j);
-                sNum++;
                 sumlist.push_back(mlist);
+                sNum++;
             }
         }
     }
-
-    for(int i=0; i< sumlist.size(); i++) {
-        vector<pair<int, int>> now = sumlist[i];
-        for(int j=0; j< now.size(); j++) {
-            int curX = now[j].first;
-            int curY = now[j].second;
-            int now_s = map[curX][curY];
-            for(int d=0; d<4; d++) {
-                int nx = curX + dx[d];
-                int ny = curY + dy[d];
-                int dist = 0;
-
-                while(nx >=0 && nx<n && ny >=0 && ny<m) {
-                    if(map[nx][ny] == now_s) { //같은 섬
+    for (int i = 0; i < sumlist.size(); i++) {
+        auto &now = sumlist[i];
+        for (auto &p : now) {
+            int x = p.first, y = p.second, id = map[x][y];
+            for (int d = 0; d < 4; d++) {
+                int nx = x + dx[d], ny = y + dy[d], dist = 0;
+                while (nx >= 0 && nx < n && ny >= 0 && ny < m) {
+                    if (map[nx][ny] == id) break;
+                    if (map[nx][ny] != 0) {
+                        if (dist > 1) pq.push(Edge{id, map[nx][ny], dist});
                         break;
                     }
-                    else if(map[nx][ny]!=0) {
-                        if(dist >1) {
-                            pq.push(Edge{now_s, map[nx][ny], dist});
-                        }
-                        break;
-                    }
-                    else {
-                        dist++;
-                    }
+                    nx += dx[d];
+                    ny += dy[d];
+                    dist++;
                 }
             }
         }
     }
-
     parent.resize(sNum);
-    for(int i=0; i<parent.size(); i++) {
-        parent[i] = i;
-    }
-
-    int useEdge = 0;//간선 카운트
-    int result = 0;
-
-    while(!pq.empty()) {
-        Edge cur = pq.top();
-        pq.pop();
-        if(find(cur.s) != find(cur.e)) {
-            munion(cur.s, cur.e);
-            result += cur.v;
-            useEdge++;
+    for (int i = 0; i < sNum; i++) parent[i] = i;
+    int used = 0, result = 0;
+    while (!pq.empty()) {
+        auto e = pq.top(); pq.pop();
+        if (find(e.s) != find(e.e)) {
+            munion(e.s, e.e);
+            result += e.v;
+            used++;
         }
     }
-
-    if(useEdge == sNum -2) {
-        cout<< result << "\n";
-    }else {
-        cout<<-1 << "\n";
-    }
+    if (used == sNum - 2) cout << result;
+    else cout << -1;
+    return 0;
 }
 
 void munion(int a, int b) {
     a = find(a);
     b = find(b);
-    if(a!=b) {
-        parent[b] = a;
-    }
+    if (a != b) parent[b] = a;
 }
 
 int find(int a) {
-    if(a==parent[a]) {
-        return a;
-    } else {
-        return parent[a] = find(parent[a]);
-    }
+    return parent[a] == a ? a : parent[a] = find(parent[a]);
 }
 
 void bfs(int i, int j) {
     queue<pair<int, int>> q;
     mlist.clear();
-    q.push(make_pair(i, j));
-    mlist.push_back(make_pair(i, j));
     visited[i][j] = true;
     map[i][j] = sNum;
-
-    while(!q.empty()) {
-        int x = q.front().first;
-        int y = q.front().second;
-        q.pop();
-
-        for(int d=0; d<4; d++) {
-            int nx = x+dx[d];
-            int ny = y+dy[d];
-            while(nx>=0 && nx<n && ny>=0 && ny<m) {
-                if(!visited[nx][ny] && map[nx][ny]!=0) {
-                    map[nx][ny] = sNum;
-                    visited[nx][ny] = true;
-                    mlist.push_back(make_pair(nx, ny));   
-                    q.push(make_pair(nx, ny));
-                }
-                else {
-                    break;
-                }
+    q.push({i, j});
+    mlist.push_back({i, j});
+    while (!q.empty()) {
+        auto [x, y] = q.front(); q.pop();
+        for (int d = 0; d < 4; d++) {
+            int nx = x + dx[d], ny = y + dy[d];
+            if (nx >= 0 && nx < n && ny >= 0 && ny < m && !visited[nx][ny] && map[nx][ny] == 1) {
+                visited[nx][ny] = true;
+                map[nx][ny] = sNum;
+                q.push({nx, ny});
+                mlist.push_back({nx, ny});
             }
         }
     }
