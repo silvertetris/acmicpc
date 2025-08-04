a4ddd95689f8b1296addd27f728ef0527d34768f
sean <dltjdrnjs3@naver.com>
2025-08-04 22:24:36 +0900

    Create 29618.kt

---
+++ src/kotlin/29618/29618.kt
@@ -0,0 +1,8 @@
+fun main() {
+    val br = System.`in`.bufferedReader()
+    val bw = System.out.bufferedWriter()
+    val (n, q) = br.readLine().split(" ").map { it.toInt() }
+    val parent = IntArray(n+1) {it}
+    val rank = IntArray(n+1) {0}
+
+}
