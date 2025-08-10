d4f5e498912be67165843f4f8d1afec72e36fc6d
sean <dltjdrnjs3@naver.com>
2025-08-10 21:47:27 +0900

    Create 1327.kt

---
+++ src/kotlin/1327/1327.kt
@@ -0,0 +1,7 @@
+fun main() {
+    val br = System.`in`.bufferedReader()
+    val(n, k) = br.readLine().split(" ").map { it.toInt() }
+    val a= br.readLine().split(" ").map { it.toInt() }.toIntArray()
+    val sorted = a.sorted()
+    
+}
