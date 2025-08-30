2d82f8e7a000b547abc35201c396a4bb5e261a85
silvertetris <dltjdrnjs3@naver.com>
2025-08-31 05:47:36 +0900

    Create 34034.kt

---
+++ src/kotlin/34034/34034.kt
@@ -0,0 +1,6 @@
+fun main() {
+    val br = System.`in`.bufferedReader()
+    val (n, m, k) = br.readLine().split(" ").map { it.toInt() }
+    val d = br.readLine().split(" ").map{ it.toInt() }.toIntArray()
+
+}
