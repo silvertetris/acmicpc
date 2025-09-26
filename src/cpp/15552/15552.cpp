da00f188c4dc34ead32d55c353888cd0941620f1
silvertetris <dltjdrnjs3@naver.com>
2025-09-27 02:27:53 +0900

    Create 15552.cpp

---
+++ src/cpp/15552/15552.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main() {
+    ios::sync_with_stdio(false);
+    int t= 0;
+    cin>>t;
+    while (t--) {
+        int a, b;
+        cin>>a>>b;
+        cout<<a+b<<endl;
+    }
+}
