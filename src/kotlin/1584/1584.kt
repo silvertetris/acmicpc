076da46d392ed11133fb3ddc9b47c875c60ef474
sean <dltjdrnjs3@naver.com>
2025-10-29 17:27:08 +0900

    Update 1584.kt

--- src/kotlin/1584/1584.kt
+++ src/kotlin/1584/1584.kt
@@ -26,7 +26,39 @@ fun main() {
         deathX2[i] = x2
         deathY2[i] = y2
     }
+
     val dx = intArrayOf(0, -1, 0, 1)
     val dy = intArrayOf(1, 0, -1, 0)
-    val queue = PriorityQueue<Pair<Pair<Int, Int>>, Int>
+    val queue = PriorityQueue<Triple<Int, Int, Int>>(compareBy { it.third })
+    val dist = Array(501) {IntArray(501) {Int.MAX_VALUE-1_000}}
+    queue.add(Triple(0,0,0))
+    while(queue.isNotEmpty()) {
+        val (curX, curY, curD) = queue.poll()
+        for(i in 0 until 4) {
+            val nx = curX + dx[i]
+            val ny = curY + dy[i]
+            if(nx !in 0 until 501 || ny !in 0 until 501) continue
+            var nd = curD
+            var deathFlag = false
+            for(i in 0 until m) {
+                if((nx in deathX1[i]..deathX2[i] || nx in deathX2[i] .. deathX1[i]) && (ny in deathY1[i]..deathY2[i] || ny in deathY2[i] .. deathY1[i])) {
+                    deathFlag = true
+                }
+            }
+            if(deathFlag) continue
+            for(i in 0 until n) {
+                if((nx in dangerX1[i]..dangerX2[i] || nx in dangerX2[i] .. dangerX1[i]) && (ny in dangerY1[i]..dangerY2[i] || ny in dangerY2[i] .. dangerY1[i])) {
+                    nd+=1
+                    break
+                }
+            }
+            if(dist[nx][ny] >nd) {
+                dist[nx][ny] = nd
+                queue.add(Triple(nx, ny, nd))
+            }
+        }
+    }
+    if(dist[500][500] > Int.MAX_VALUE-1_000_000) {
+        print(-1)
+    } else print(dist[500][500])
 }
