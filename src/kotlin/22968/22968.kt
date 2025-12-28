dfa8f1d9fa5fcde65bef517edae4e141b3cdfc33
sean <dltjdrnjs3@naver.com>
2025-12-29 00:14:53 +0900

    Update 22968.kt

--- src/kotlin/22968/22968.kt
+++ src/kotlin/22968/22968.kt
@@ -1,20 +1,18 @@
-
-
 fun main() {
     val br = System.`in`.bufferedReader()
     val bw= System.out.bufferedWriter()
-    val dp = LongArray(44) {0}
-    dp[0] = 1
-    dp[1] = 3
-    for(i in 2 .. 43) {
-        dp[i] = dp[i-1] + (dp[i-1]-dp[i-2])*2
+    val dp = LongArray(45) {0}
+    dp[0] = 0
+    dp[1] = 1
+    for(i in 2..44) {
+        dp[i] = dp[i-1] + dp[i-2] +1
     }
     val t = br.readLine().toInt()
     repeat(t) {
         val v = br.readLine().toLong()
-        for(i in 0 .. 43) {
-            if(dp[i]>=v) {
-                bw.write("${i+1}\n")
+        for(i in 1 .. 43) {
+            if(dp[i]>v) {
+                bw.write("${i-1}\n")
                 break
             }
         }
