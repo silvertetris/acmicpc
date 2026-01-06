ea8f5c0cfc8425c3688a0c714aeb91ec0d4d4d77
sean <dltjdrnjs3@naver.com>
2026-01-06 19:12:56 +0900

    Create 1896.kt

---
+++ src/kotlin/1896/1896.kt
@@ -0,0 +1,76 @@
+fun main() {
+    val br = System.`in`.bufferedReader()
+    val bw = System.out.bufferedWriter()
+    val t = br.readLine().toInt()
+    val dx = intArrayOf(0, -1, 0, 1)
+    val dy = intArrayOf(-1, 0, 1, 0)
+    repeat(t) {
+        val (r, c) = br.readLine().split(" ").map{it.toInt()}
+        val poly = Array(r) {br.readLine().toCharArray()}
+        val descr = Array(r) {br.readLine().split(" ").filter{it.isNotEmpty()}.map{it.toInt()}.toIntArray()}
+        val idxList = mutableListOf<MutableList<Int>>()
+        val idx = mutableListOf<Int>()
+        val visited = Array(r) {BooleanArray(c) {false}}
+        fun dfs(x:Int, y:Int) {
+            visited[x][y] = true
+            idx.add(poly[x][y]-'0')
+            val checkNum = Integer.toBinaryString(descr[x][y]).toString()
+            for(i in checkNum.lastIndex downTo 0) {
+                val nx = x+dx[i]
+                val ny = y+dy[i]
+                if(nx in 0 until r && ny in 0 until c && !visited[nx][ny]) {
+                    dfs(nx, ny)
+                }
+            }
+        }
+        //dfs
+        for(i in 0 until r) {
+            for(j in 0 until c) {
+                if(!visited[i][j]) {
+                    dfs(i, j)
+                    idxList.add(idx.toMutableList())
+                    idx.clear()
+                }
+            }
+        }
+        for(i in idxList.indices) {
+            idxList[i].sort()
+            for(j in 0 until idxList[i].size) {
+                if(j+1 != idxList[i][j]) {
+                    bw.write("invalid\n")
+                    return@repeat
+                }
+            }
+        }
+        for(i in 0 until r) {
+            for(j in 0 until c) {
+                val num = poly[i][j]-'0'//거점
+                //4방향으로 뻗기, i, j 에서 덧셈 뺄셈 -> 4가지 경우의 수
+                for(k in 1 .. num) {
+                    val up = i+k
+                    val down = i-k
+                    val left = j-k
+                    val right = j+k
+                    if(up in 0 until r && poly[up][j] == poly[i][j]) {
+                        bw.write("invalid\n")
+                        return@repeat
+                    }
+                    if(down in 0 until r && poly[down][j] == poly[i][j]) {
+                        bw.write("invalid\n")
+                        return@repeat
+                    }
+                    if(left in 0 until c && poly[i][left] == poly[i][j]) {
+                        bw.write("invalid\n")
+                        return@repeat
+                    }
+                    if(right in 0 until c && poly[i][right] == poly[i][j]) {
+                        bw.write("invalid\n")
+                        return@repeat
+                    }
+                }
+            }
+        }
+        bw.write("valid\n")
+    }
+    bw.flush()
+}
