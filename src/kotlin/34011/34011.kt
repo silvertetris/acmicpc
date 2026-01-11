bc56248e237dc9f432e628eddcadd3d4274ea998
sean <dltjdrnjs3@naver.com>
2026-01-12 04:09:07 +0900

    Update 34011.kt

--- src/kotlin/34011/34011.kt
+++ src/kotlin/34011/34011.kt
@@ -15,17 +15,21 @@ fun main() {
     /*
     리프 노드 안되고
     깊이가 2이상인거만
+    이고 -> 원하는 횟수만큼 계속해서 이동할 수 있다는 거임 제동계수 의 배수
+    그니까 2부터 ... N의 배수까지 이동할 수 있음
+    근데 N거리만큼 이동하는건 현실적으로 불가능
+    그니까 최대 n-2까지는 이동이됨 (리프제외니까)
+
      */
-    var cnt = 1
     val visited = BooleanArray(n+1) {false}
+    var cnt = 1
     queue.add(1 to 1)
     visited[1] = true
     while(queue.isNotEmpty()) {
         val size = queue.size
-
         repeat(size) {
             val(x, y) = queue.poll()
-            if(y>2 && x!=1 && graph[x].size!=1) cnt++
+            cnt = maxOf(cnt, y)
             for(i in graph[x]) {
                 if(!visited[i]) {
                     visited[i] = true
@@ -34,5 +38,9 @@ fun main() {
             }
         }
     }
-    print(cnt)
+    if(cnt<3) {
+        print(1)
+    }else {
+        print(cnt-2)
+    }
 }
