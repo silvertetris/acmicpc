b6c790ef2b2b2a8f5ba43cd165a967dc4ecdf323
sean <dltjdrnjs3@naver.com>
2026-01-26 21:32:19 +0900

    Create 9082.kt

---
+++ src/kotlin/9082/9082.kt
@@ -0,0 +1,15 @@
+fun main() {
+    val br = System.`in`.bufferedReader()
+    val bw = System.out.bufferedWriter()
+
+    val t = br.readLine().toInt()
+    repeat(t) {
+        val n = br.readLine().toInt()
+        val num = br.readLine()
+        val field = br.readLine()
+        var res = 0
+        for(i in 0 until n-1) {
+
+        }
+    }
+}
