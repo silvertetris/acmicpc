54bb84aa1685c2fd759ef846b4a61f121248e4cc
silvertetris <dltjdrnjs3@naver.com>
2026-02-05 02:15:12 +0900

    Update 33692.kt

--- src/kotlin/33692/33692.kt
+++ src/kotlin/33692/33692.kt
@@ -6,7 +6,6 @@ fun main() {
     val sbA = StringBuilder(binA)//min
     val sbB = StringBuilder(binB)//max
     var flag = false
-    var able = false
     if (binA.length < binB.length) {
         val diff = sbB.length - sbA.length
         for (i in 0 until diff) {
@@ -17,19 +16,28 @@ fun main() {
             }
         }
         for (i in 0 until sbA.length) {
-            if (sbA[i] == sbB[i]) {
-                sbB[i] = if (sbA[i] == '0') '1' else '0'
+            //이것도 max가 더 올려질 수도 있음
+            if(sbA[i]=='1' && sbB[i]=='1') {
+                sbB[i] = '0'
+            }
+            else if(sbA[i]=='0' && sbB[i]=='0') {
+                sbA[i]='1'
             }
         }
     } else {
         for (i in 0 until sbA.length) {
             if (!flag && sbA[i] == sbB[i]) {
                 continue
-            } else if (!flag && sbA[i] != sbB[i]) {
-                flag = true
-            } else if (flag && sbA[i] == sbB[i] &&!able) {
-                sbA[i] = if (sbB[i] == '0') '1' else '0'
+            }else if(!flag && sbB[i]!=sbA[i]) {//이 경우는 항상 sbB가 큼 그니까 이 이후에는 sbA를 올리는 방식이여야함 (sbB=1, sbA=0
+                flag= true
             }
+            else if(flag && sbA[i]=='0' &&sbB[i]=='0') {
+                sbA[i]='1'
+            }
+            else if(flag && sbA[i]=='1' && sbB[i] =='1') {
+                sbB[i]='0'
+            }
+
         }
     }
     print("${sbA.toString().toLong(2)} ${sbB.toString().toLong(2)}")
