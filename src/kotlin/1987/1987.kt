a5fc7e3bcca99de2a860eef417b366a579ca483e
sean <dltjdrnjs3@naver.com>
2026-02-25 23:12:50 +0900

    Create 1987.kt

---
+++ src/kotlin/1987/1987.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val br = System.`in`.bufferedReader()
+    val (r, c) = br.readLine().split(" ").map { it.toInt() }
+    val board = Array(r) { br.readLine() }
+    var res = 0
+    val visited = Array(r) { BooleanArray(c) }
+    val dup = mutableSetO
+    fun dfs(x: Int, y: Int, depth: Int) {
+
+    }
+}
