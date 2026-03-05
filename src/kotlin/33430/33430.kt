2f6bac9b0da8cb52db57e411404c8eeeb6e42fd0
sean <dltjdrnjs3@naver.com>
2026-03-05 19:48:04 +0900

    Create 33430.kt

---
+++ src/kotlin/33430/33430.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val br = System.`in`.bufferedReader()
+    val bw = System.out.bufferedWriter()
+    val t = br.readLine().toInt()
+    repeat(t) {
+        val n = br.readLine().toInt()
+
+    }
+}
