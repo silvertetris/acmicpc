6775fc0d1b62687769401edfe78f8c88eb47bf26
sean <dltjdrnjs3@naver.com>
2026-03-12 16:31:30 +0900

    Update 32160.kt

--- src/kotlin/32160/32160.kt
+++ src/kotlin/32160/32160.kt
@@ -1,5 +1,48 @@
 fun main() {
+    val br = System.`in`.bufferedReader()
+    val n = br.readLine().toInt()
     val bw = System.out.bufferedWriter()
-    val n = readln().toInt()
-    
+
+    var cnt = 0
+    if (n == 2) {
+        bw.write("1\n2 1")
+        bw.flush()
+        return
+    }
+    if ((n / 2) % 2 == 0) {
+        bw.write("$n\n")
+    } else {
+        bw.write("${n - 1}\n")
+    }
+    for (i in n - 2 downTo 1 step 2) {
+        bw.write("$i ${i + 1}\n")
+        cnt++
+    }
+    if(n%2==0) {
+        cnt++
+    }
+    var temp = 0
+    for (i in 0 until cnt/2) {//여기가 문제
+        bw.write("1 1\n")
+        temp++
+    }
+    if(n%2==0) {
+        temp++
+    }
+    if (temp % 2 == 0) {
+        for (i in 0 until temp/2) {
+            bw.write("0 0\n")
+        }
+    }
+    else {
+        for(i in 0 until temp/2) {
+            bw.write("1 0\n")
+        }
+    }
+    if (temp % 2 == 0) {
+        bw.write("0 $n")
+    } else {
+        bw.write("1 $n")
+    }
+    bw.flush()
 }
