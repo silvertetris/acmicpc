fed9dbb0a30d73819b66d8c45fba5db866ca6c74
silvertetris <dltjdrnjs3@naver.com>
2026-04-05 22:26:05 +0900

    Update 14653.kt

--- src/kotlin/14653/14653.kt
+++ src/kotlin/14653/14653.kt
@@ -1,4 +1,36 @@
 fun main() {
     val br = System.`in`.bufferedReader()
     val(n, k, q) = br.readLine().split(" ").map { it.toInt() }
+    val nums = IntArray(k)
+    val peps = CharArray(k)
+    val check = BooleanArray(n) {false}
+    check[0] = true
+    var cnt = 1
+    var qNum = 0
+    for(i in 0 until k) {
+        val line = br.readLine().split(" ")
+        nums[i] = line[0].toInt()
+        peps[i] = line[1][0]
+        if(i==q-1) {
+            qNum = nums[i]
+        }
+    }
+    if(qNum==0) {
+        print(-1)
+        return
+    }
+    for(i in k-1 downTo 0) {
+        if(nums[i]<qNum) {
+            break
+        }
+        val temp = peps[i].code-65
+        if(!check[temp]) {
+            check[temp] = true
+        }
+    }
+    for(i in 0 until n) {
+        if(!check[i]) {
+            print("${(i+65).toChar()} ")
+        }
+    }
 }
