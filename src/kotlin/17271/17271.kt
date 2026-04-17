5a089da24701b70ee8473d5377b0a4d50109aa3c
silvertetris <dltjdrnjs3@naver.com>
2026-04-18 04:06:24 +0900

    Update 17271.kt

--- src/kotlin/17271/17271.kt
+++ src/kotlin/17271/17271.kt
@@ -4,12 +4,13 @@ fun main() {
     A는 1초고 B는 M초임
     싸움 시간 N
      */
-    val dp = Array(n+1) {IntArray(2) {0} }
-    for(i in 1 .. n) {
-        if(i%m==0) {
-            dp[i][0] += dp[i/m][1] +1
+    val dp = IntArray(n+1){0}
+    dp[0] = 1
+    for(i in 1..n) {
+        dp[i] = dp[i-1]
+        if(i>=m) {
+            dp[i] = (dp[i]+dp[i-m])%1_000_000_007
         }
-        dp[i][0] = dp[i][0] + dp[i-1][0]
     }
-    print(dp[n][0] + dp[n][1])
+    print(dp[n])
 }
